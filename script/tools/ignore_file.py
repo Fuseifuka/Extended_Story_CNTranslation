a70e91c7f9df4b2b4c6bdba14107892761448234
FUSHImissioanry <1409954307@qq.com>
2020-05-19 17:19:56 +0800

    第一次提交

---
+++ script/tools/ignore_file.py
@@ -0,0 +1,14 @@
+# Ignore file list
+
+ignore_filelist = [
+    'teslagun.activeitem',
+    'teslagun2.activeitem',
+    'converse.config.json'
+    'entries.config',
+    'electricartifact.activeitem',
+    'electricartifact.animation',
+    'staffglow.frames'
+]
+ignore_filelist_patch = [
+    
+]
